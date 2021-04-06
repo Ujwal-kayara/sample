8f3e02976d7c4deca5b19aca572eefd16ba6006f
Ujwal-kayara <82054592+Ujwal-kayara@users.noreply.github.com>
2021-04-06 23:46:30 +0530

    Add files via upload

---
+++ sample_p.py
@@ -0,0 +1,7 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print"hello"
+hello
+>>> print"This is my first project"
+This is my first project
+>>> 
