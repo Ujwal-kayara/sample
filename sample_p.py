ce2180a387a5493d9cd4d43f57a1930f19e02995
Ujwal <ujwalkayar42@gmail.com>
2021-06-16 23:08:18 +0530

    adding fist_file and sample_p contents

--- sample_p.py
+++ sample_p.py
@@ -1,7 +1,5 @@
-Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print"hello"
-hello
->>> print"This is my first project"
-This is my first project
->>> 
+import sys
+import DateTime
+
+print time.time()
+
